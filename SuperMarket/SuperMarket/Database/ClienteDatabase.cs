d81c6575de6d72191c20d055dc803db0a9314dcf
oficina2019 <oficinansf2019@gmail.com>
2020-03-27 00:34:25 -0300

    uuuu teste

---
+++ SuperMarket/SuperMarket/Database/ClienteDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Database
+{
+    class ClienteDatabase
+    {
+        Models.supermarket_dbEntities db = new Models.supermarket_dbEntities();
+
+        public void Insert(Models.tb_cliente cliente)
+        {
+            db.tb_cliente.Add(cliente);
+            db.SaveChanges();
+        }
+        public void InsertEndereco(Models.tb_enderecocliente endereco)
+        {
+            db.tb_enderecocliente.Add(endereco);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ SuperMarket/SuperMarket/Database/ProdutoDatabase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Database
+{
+    class ProdutoDatabase
+    {
+        Models.supermarket_dbEntities db = new Models.supermarket_dbEntities();
+
+        public void Insert (Models.tb_produto produto)
+        {
+            db.tb_produto.Add(produto);
+            db.SaveChanges();
+        }
+        public List<Models.tb_produto> ListarTodos()
+        {
+            List<Models.tb_produto> lista = db.tb_produto.ToList();
+            return lista;
+        }
+        public List<Models.tb_produto> FiltrarPorNome(string nome)
+        {
+            List<Models.tb_produto> lista = db.tb_produto.Where(t => t.nm_produto == nome)
+                                                         .OrderBy(t => t.nm_produto)
+                                                         .ToList();
+            return lista;
+        }
+        public void Alterar (Models.tb_produto produto)
+        {
+            Models.tb_produto alterar = db.tb_produto
+                .FirstOrDefault(t => t.idtb_produto == produto.idtb_produto);
+            
+            if(alterar != null)
+            {
+                alterar.ds_origem = produto.ds_origem;
+                alterar.dt_fabricacao = produto.dt_fabricacao;
+                alterar.dt_validade = produto.dt_validade;
+                alterar.nm_produto = produto.nm_produto;
+                alterar.vl_venda = produto.vl_venda;
+            }
+            db.SaveChanges();
+        }
+        public void Remover (int id)
+        {
+            Models.tb_produto remover = db.tb_produto.
+                FirstOrDefault(t => t.idtb_produto == id);
+            db.tb_produto.Remove(remover);
+            db.SaveChanges();
+        }
+
+    }
+}
